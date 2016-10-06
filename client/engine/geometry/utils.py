1cccf8cdd69346ef27d3f7b4a472dc1b27a2fa26
radiantdawn <true.feanor@gmail.com>
2016-10-06 11:29:19 +0300

    Incircle
    Added incircle (a, b, c, d) function, which determines the position of
    point d relative to the circle defined by points a, b, and c.

--- client/engine/geometry/utils.py
+++ client/engine/geometry/utils.py
@@ -1,3 +1,23 @@
+def det(matrix, mul):
+    width = len(matrix)
+    if width == 1:
+        return mul * matrix[0][0]
+    else:
+        sign = -1
+        total = 0
+        for i in range(width):
+            m = []
+            for j in range(1, width):
+                buff = []
+                for k in range(width):
+                    if k != i:
+                        buff.append(matrix[j][k])
+                m.append(buff)
+            sign *= -1
+            total += mul * det(m, sign * matrix[0][i])
+        return total
+
+
 def orient(a, b, c):
 
     "Determines the position of point c relative to the directed line\
@@ -6,4 +26,28 @@ def orient(a, b, c):
     if less than 0, it is to the right,\
     and 0 means it is located on the line."
 
-    return a.x*(b.y - c.y) - a.y*(b.x - c.x) + b.x*c.y - b.y*c.x
+    return a.x * (b.y - c.y) - a.y * (b.x - c.x) + b.x * c.y - b.y * c.x
+
+
+def incircle(a, b, c, d):
+
+    "Determines whether point d lies within the circle formed \
+    by points a, b, and c.\
+    If the return value is greater than 0, point d is inside the circle,\
+    if less than 0, it is outside, and 0 means the four points are cocircular."
+
+    ort = orient(a, b, c)
+
+    if (ort == 0):
+        raise ValueError("Points a, b, and c are colinear and cannot form\
+            a circle.")
+    elif (ort > 0):
+        return det([[a.x, a.y, a.x * a.x + a.y * a.y, 1],
+                    [b.x, b.y, b.x * b.x + b.y * b.y, 1],
+                    [c.x, c.y, c.x * c.x + c.y * c.y, 1],
+                    [d.x, d.y, d.x * d.x + d.y * d.y, 1]], 1)
+    else:
+        return ((-1) * det([[a.x, a.y, a.x * a.x + a.y * a.y, 1],
+                            [b.x, b.y, b.x * b.x + b.y * b.y, 1],
+                            [c.x, c.y, c.x * c.x + c.y * c.y, 1],
+                            [d.x, d.y, d.x * d.x + d.y * d.y, 1]], 1))
--- client/tests/testgeometry/test_utils.py
+++ client/tests/testgeometry/test_utils.py
@@ -2,6 +2,7 @@ import unittest
 
 from engine.geometry import Vector
 from engine.geometry.utils import orient
+from engine.geometry.utils import incircle
 
 
 class TestUtils(unittest.TestCase):
@@ -15,5 +16,15 @@ class TestUtils(unittest.TestCase):
         self.assertLess(orient(a, b, Vector(.745, .0005)), 0)
         self.assertLess(orient(Vector(0, 1), Vector(11, -2), Vector(1, -4)), 0)
         a2 = Vector(0.0000003, 0.0000006)
-        b2 = Vector(0.00000030001, 0.0000006)
+        b2 = Vector(0.000000301001, 0.0000006)
         self.assertGreater(orient(a2, b2, Vector(5, 5)), 0)
+
+    def test_incircle(self):
+        a = Vector(0, 0)
+        b = Vector(2, 0)
+        c = Vector(1, 1)
+        self.assertGreater(incircle(a, b, c, Vector(1, 0)), 0)
+        self.assertGreater(incircle(a, b, c, Vector(1, 0.9999999991999)), 0)
+        self.assertLess(incircle(a, b, c, Vector(-11, 0)), 0)
+        self.assertEqual(incircle(a, b, c, Vector(1, 1)), 0)
+        self.assertEqual(incircle(a, b, c, Vector(1, -1)), 0)
