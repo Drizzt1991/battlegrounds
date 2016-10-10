0bd76f71246bacb70cd063db4f0b21101fb8c762
Taras <voyn1991@gmail.com>
2016-10-10 17:07:39 +0300

    Apply movement by a constant dt periods

--- client/client/world/world.py
+++ client/client/world/world.py
@@ -11,10 +11,18 @@ class World(ABCWorld):
         self._props = load_props(self, world_map)
         self._actors = [
             Character(self, position=Vector(0, 0))]
+        self._tick_period = 0.03125  # ~30 fps simulation
+        self._timer = 0
+        self._reminder = 0
 
     def tick(self, dt):
-        for actor in self._actors:
-            actor.tick(dt)
+        period = self._tick_period
+        dt = self._reminder + dt
+        self._reminder = dt % period
+        for _ in range(int(dt // period)):
+            for actor in self._actors:
+                actor.tick(period)
+            self._timer += period
 
     @property
     def props(self):
