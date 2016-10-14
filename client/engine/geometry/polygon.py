a2e4cbdd7153ef676eb487b4d6b280c7ebe7a5ac
radiantdawn <true.feanor@gmail.com>
2016-10-14 13:25:05 +0300

    SAT
    Added polygon intersection check, tests.

--- client/engine/geometry/polygon.py
+++ client/engine/geometry/polygon.py
@@ -1,6 +1,6 @@
 from .shape import BaseIntersection, BaseShape
-from .vector import Vector
 from .utils import orient
+from .vector import Vector
 
 
 class TriangleIntersection(BaseIntersection):
@@ -86,3 +86,69 @@ class Triangle(BaseShape):
         if other is x:
             return -1
         return x.distance(other)
+
+
+class PolygonIntersection(BaseIntersection):
+    pass
+
+
+class Polygon(BaseShape):
+    """(Assuming that the polygon is convex and ordered counter-clockwise)"""
+
+    def __init__(self, points):
+        assert len(points) >= 3
+        self._points = points
+
+    @property
+    def points(self):
+        return self._points
+
+    def contains(self, other):
+        assert isinstance(other, Vector)
+        lo = 0
+        hi = len(self.points)
+        while (lo + 1 < hi):
+            mid = (lo + hi) // 2
+            if orient(self.points[0], self.points[mid], other) == 0:
+                lx = abs(other.x - self.points[0].x)
+                lx += abs(self.points[mid].x - other.x)
+                ly = abs(other.y - self.points[0].y)
+                ly += abs(self.points[mid].y - other.y)
+                if (abs(self.points[mid].x - self.points[0].x) == lx and
+                    abs(self.points[mid].y - self.points[0].y) == ly):  # noqa
+                    return True
+                else:
+                    return False
+            elif orient(self.points[0], self.points[mid], other) < 0:
+                hi = mid
+            else:
+                lo = mid
+        if (lo == 0 or hi == len(self.points)):
+            return False
+        if orient(self.points[lo], self.points[hi], other) < 0:
+            return False
+        return True
+
+    def intersects(self, other):
+        assert isinstance(other, Polygon)
+        ns = []  # list of normals to each edge of both polygons
+        for i in range(0, len(self.points) - 1):
+            ns.append(Vector(self.points[i + 1].y - self.points[i].y,
+                             self.points[i].x - self.points[i + 1].x))
+        ns.append(Vector(self.points[i].y - self.points[0].y,
+                         self.points[0].x - self.points[i].x))
+        for i in range(0, len(other.points) - 1):
+            ns.append(Vector(other.points[i + 1].y - other.points[i].y,
+                             other.points[i].x - other.points[i + 1].x))
+        ns.append(Vector(other.points[i].y - other.points[0].y,
+                         other.points[0].x - other.points[i].x))
+        for n in ns:
+            sp = []  # projections of self's vertexes onto line n
+            for v in self.points:
+                sp.append(v.dot(n))
+            op = []  # projections of other's vertexes onto line n
+            for v in other.points:
+                op.append(v.dot(n))
+            if max(op) < min(sp) or min(op) > max(sp):
+                return None
+        return PolygonIntersection(self, other)
